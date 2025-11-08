c783aea4e0b16659289c1a72469a655043d88126
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:55:27 -0500

    feat: implement appointment creation logic in AppointmentCommandService

--- neurozen.API/Appointments/Application/Internal/CommandServices/AppointmentCommandService.cs
+++ neurozen.API/Appointments/Application/Internal/CommandServices/AppointmentCommandService.cs
@@ -1,6 +1,25 @@
-﻿namespace neurozen.API.Appointments.Application.Internal.CommandServices;
+﻿using neurozen.API.Appointments.Domain.Model.Aggregates;
+using neurozen.API.Appointments.Domain.Model.Commands;
+using neurozen.API.Appointments.Domain.Repositories;
+using neurozen.API.Appointments.Domain.Services;
+using neurozen.API.Shared.Domain.Repositories;
 
-public class AppointmentCommandService
+namespace neurozen.API.Appointments.Application.Internal.CommandServices;
+
+public class AppointmentCommandService(IAppointmentRepository appointmentRepository, IUnitOfWork unitOfWork) : IAppointmentCommandService
 {
-    
+    public async Task<Appointment?> Handle(CreateAppointmentCommand command)
+    {
+        var appointment = new Appointment(command);
+        try
+        {
+            await appointmentRepository.AddAsync(appointment);
+            await unitOfWork.CompleteAsync();
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+        return appointment;
+    }
 }
