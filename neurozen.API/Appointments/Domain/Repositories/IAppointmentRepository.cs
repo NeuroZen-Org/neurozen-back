09142041ce6ac3d3aacacf179558951df6016dfd
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:21:45 -0500

    feat: extend IAppointmentRepository to include method for querying appointments by patient ID

--- neurozen.API/Appointments/Domain/Repositories/IAppointmentRepository.cs
+++ neurozen.API/Appointments/Domain/Repositories/IAppointmentRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace neurozen.API.Appointments.Domain.Repositories;
+﻿using neurozen.API.Appointments.Domain.Model.Aggregates;
+using neurozen.API.Shared.Domain.Repositories;
 
-public interface IAppointmentRepository
+namespace neurozen.API.Appointments.Domain.Repositories;
+
+public interface IAppointmentRepository : IBaseRepository<Appointment>
 {
-    
+    /// <summary>
+    ///     Get all appointments by patient id and professional id
+    /// </summary>
+    /// <returns>An enumerable with all appointments</returns>
+    Task<IEnumerable<Appointment>> GetAllAppointmentsQueryByPatientId(int patientId);
 }
