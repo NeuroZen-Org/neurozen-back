caecaf990183520e6d2dc5410b9daabbccb6ced2
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:19:57 -0500

    feat: implement appointment retrieval by patient ID in query and repository services

--- neurozen.API/Appointments/Application/Internal/QueryServices/AppointmentQueryService.cs
+++ neurozen.API/Appointments/Application/Internal/QueryServices/AppointmentQueryService.cs
@@ -1,6 +1,14 @@
-﻿namespace neurozen.API.Appointments.Application.Internal.QueryServices;
+﻿using neurozen.API.Appointments.Domain.Model.Aggregates;
+using neurozen.API.Appointments.Domain.Model.Queries;
+using neurozen.API.Appointments.Domain.Repositories;
+using neurozen.API.Appointments.Domain.Services;
 
-public class AppointmentQueryService
+namespace neurozen.API.Appointments.Application.Internal.QueryServices;
+
+public class AppointmentQueryService(IAppointmentRepository appointmentRepository) : IAppointmentQueryService
 {
-    
+    public async Task<IEnumerable<Appointment>> Handle(GetAllAppointmentsQueryByPatientId query)
+    {
+        return await appointmentRepository.GetAllAppointmentsQueryByPatientId(query.PatientId);
+    }
 }
--- neurozen.API/Appointments/Infrastructure/Repositories/AppointmentRepository.cs
+++ neurozen.API/Appointments/Infrastructure/Repositories/AppointmentRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace neurozen.API.Appointments.Infrastructure.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using neurozen.API.Appointments.Domain.Model.Aggregates;
+using neurozen.API.Appointments.Domain.Model.Queries;
+using neurozen.API.Appointments.Domain.Repositories;
+using neurozen.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using neurozen.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 
-public class AppointmentRepository
+namespace neurozen.API.Appointments.Infrastructure.Repositories;
+
+public class AppointmentRepository(AppDbContext context) : BaseRepository<Appointment>(context), IAppointmentRepository
 {
-    
+    public async Task<IEnumerable<Appointment>> GetAllAppointmentsQueryByPatientId(int patientId)
+    {
+        return await context.Set<Appointment>()
+            .Where(a => a.PatientId == patientId)
+            .ToListAsync();
+    }
 }
