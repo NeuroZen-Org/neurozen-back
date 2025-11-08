6e05d35e05cbf95afc82c05d124a6e2a2af2b8e6
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:19:30 -0500

    feat: update AppointmentAudit model with new properties and data types

--- neurozen.API/Appointments/Domain/Model/Aggregates/AppointmentAudit.cs
+++ neurozen.API/Appointments/Domain/Model/Aggregates/AppointmentAudit.cs
@@ -1,12 +1,13 @@
-﻿using neurozen.API.Shared.Domain.Repositories;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using neurozen.API.Shared.Domain.Repositories;
 
 namespace neurozen.API.Appointments.Domain.Model.Aggregates;
 
 public partial class AppointmentAudit : IEntityWithCreatedUpdatedDated
 {
-    public int Id { get; set; }
-    public int AppointmentId { get; set; }
-    public string Action { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    [Column ("CreatedAt")] public DateTimeOffset CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset UpdatedDate { get; set; }
+    [Column ("PatientId")] public long PatientId { get; private set; }
+    [Column ("ProfessionalId")] public long ProfessionalId { get; private set; }
+    [Column ("AppointmentDate")] public DateTime AppointmentDate { get; private set; }
 }
