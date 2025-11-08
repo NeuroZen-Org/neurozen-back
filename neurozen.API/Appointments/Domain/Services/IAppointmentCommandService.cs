2557ada422b9893a4e39b5089aabbe3929c1a3db
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:55:13 -0500

    feat: refactor CreateAppointmentCommand to use record type and update assembler method

--- neurozen.API/Appointments/Domain/Services/IAppointmentCommandService.cs
+++ neurozen.API/Appointments/Domain/Services/IAppointmentCommandService.cs
@@ -1,6 +1,9 @@
-﻿namespace neurozen.API.Appointments.Domain.Services;
+﻿using neurozen.API.Appointments.Domain.Model.Aggregates;
+using neurozen.API.Appointments.Domain.Model.Commands;
+
+namespace neurozen.API.Appointments.Domain.Services;
 
 public interface IAppointmentCommandService
 {
-    
+    Task<Appointment?> Handle(CreateAppointmentCommand command);
 }
--- neurozen.API/Appointments/Interfaces/REST/Transform/CreateAppointmentCommandFromResourceAssembler.cs
+++ neurozen.API/Appointments/Interfaces/REST/Transform/CreateAppointmentCommandFromResourceAssembler.cs
@@ -1,6 +1,10 @@
-﻿namespace neurozen.API.Appointments.Interfaces.REST.Transform;
+﻿using neurozen.API.Appointments.Domain.Model.Commands;
+using neurozen.API.Appointments.Interfaces.REST.Resources;
+
+namespace neurozen.API.Appointments.Interfaces.REST.Transform;
 
 public class CreateAppointmentCommandFromResourceAssembler
 {
-    
+    public static CreateAppointmentCommand ToCommandFromResource(CreateAppointmentResource resource) =>
+        new CreateAppointmentCommand(resource.PatientId, resource.ProfessionalId, resource.AppointmentDateTime);
 }
