e1336578c449f460f9ac260167f6e4ee597d912a
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:20:10 -0500

    feat: enhance AppointmentResource with properties and add assembler method for entity conversion

--- neurozen.API/Appointments/Interfaces/REST/Resources/AppointmentResource.cs
+++ neurozen.API/Appointments/Interfaces/REST/Resources/AppointmentResource.cs
@@ -1,3 +1,3 @@
 ﻿namespace neurozen.API.Appointments.Interfaces.REST.Resources;
 
-public record AppointmentResource();
+public record AppointmentResource(int Id, long PatientId, long ProfessionalId, DateTime AppointmentDateTime);
--- neurozen.API/Appointments/Interfaces/REST/Transform/AppointmentResourceFromEntityAssembler.cs
+++ neurozen.API/Appointments/Interfaces/REST/Transform/AppointmentResourceFromEntityAssembler.cs
@@ -1,6 +1,15 @@
-﻿namespace neurozen.API.Appointments.Interfaces.REST.Transform;
+﻿using neurozen.API.Appointments.Domain.Model.Aggregates;
+using neurozen.API.Appointments.Interfaces.REST.Resources;
+
+namespace neurozen.API.Appointments.Interfaces.REST.Transform;
 
 public class AppointmentResourceFromEntityAssembler
 {
-    
+    public static AppointmentResource ToResourceFromEntity(Appointment entity)=>
+        new AppointmentResource(
+            entity.Id,
+            entity.PatientId,
+            entity.ProfessionalId,
+            entity.AppointmentDateTime
+        );
 }
