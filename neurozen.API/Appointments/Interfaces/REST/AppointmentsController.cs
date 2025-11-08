dfe570b72bca6cee79160d35870d7f7e80c9a352
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:21:51 -0500

    feat: add endpoint to retrieve appointments by patient ID

--- neurozen.API/Appointments/Interfaces/REST/AppointmentsController.cs
+++ neurozen.API/Appointments/Interfaces/REST/AppointmentsController.cs
@@ -1,6 +1,40 @@
-﻿namespace neurozen.API.Appointments.Interfaces.REST;
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using neurozen.API.Appointments.Domain.Model.Queries;
+using neurozen.API.Appointments.Domain.Services;
+using neurozen.API.Appointments.Interfaces.REST.Resources;
+using neurozen.API.Appointments.Interfaces.REST.Transform;
 
-public class AppointmentsController
+namespace neurozen.API.Appointments.Interfaces.REST;
+
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[Tags("Appointments")]
+public class AppointmentsController(
+    IAppointmentCommandService appointmentCommandService,
+    IAppointmentQueryService appointmentQueryService) : ControllerBase
 {
     
+    
+    
+    
+    
+    
+    [HttpGet("appointments/{patientId}")]
+    [SwaggerOperation(
+        Summary = "Get all appointments by patient ID",
+        Description = "Retrieves all appointments associated with a specific patient ID."
+    )]
+    [SwaggerResponse(200, "Appointments retrieved successfully", typeof(IEnumerable<AppointmentResource>))]
+    [SwaggerResponse(400, "Appointments retrieval failed")]
+    public async Task<ActionResult<IEnumerable<AppointmentResource>>> GetAllAppointmentsByPatientIdAsync(int patientId)
+    {
+        var getAllAppointmentsByPatientIdQuery = new GetAllAppointmentsQueryByPatientId(patientId);
+        var result = await appointmentQueryService.Handle(getAllAppointmentsByPatientIdQuery);
+        var appointments = result.Select(AppointmentResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(appointments);
+    }
 }
