c74fadd43c86369ef72dd99b391b84677aeb4068
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 19:55:58 -0500

    feat: implement CreateAppointment and GetAppointmentById methods in AppointmentsController

--- neurozen.API/Appointments/Interfaces/REST/AppointmentsController.cs
+++ neurozen.API/Appointments/Interfaces/REST/AppointmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
+using neurozen.API.Appointments.Domain.Model.Commands;
 using Swashbuckle.AspNetCore.Annotations;
 using neurozen.API.Appointments.Domain.Model.Queries;
 using neurozen.API.Appointments.Domain.Services;
@@ -17,11 +18,20 @@ public class AppointmentsController(
     IAppointmentCommandService appointmentCommandService,
     IAppointmentQueryService appointmentQueryService) : ControllerBase
 {
-    
-    
-    
-    
-    
+    [HttpPost]
+    [SwaggerOperation(
+        Summary = "Create a new appointment",
+        Description = "Creates a new appointment with the provided details."
+    )]
+    [SwaggerResponse(201, "Appointment created successfully", typeof(AppointmentResource))]
+    [SwaggerResponse(400, "Appointment creation failed")]
+    public async Task<ActionResult> CreateAppointment([FromBody] CreateAppointmentResource resource)
+    {
+        var createAppointmentCommand = CreateAppointmentCommandFromResourceAssembler.ToCommandFromResource(resource);
+        var result =  await appointmentCommandService.Handle(createAppointmentCommand);
+        if (result is null) return BadRequest();
+        return CreatedAtAction(nameof(GetAppointmentById), new{id = result.Id}, AppointmentResourceFromEntityAssembler.ToResourceFromEntity(result));
+    }
     
     [HttpGet("appointments/{patientId}")]
     [SwaggerOperation(
@@ -36,5 +46,19 @@ public class AppointmentsController(
         var result = await appointmentQueryService.Handle(getAllAppointmentsByPatientIdQuery);
         var appointments = result.Select(AppointmentResourceFromEntityAssembler.ToResourceFromEntity);
         return Ok(appointments);
+    }
+    
+    [HttpGet("{id}")]
+    [SwaggerOperation(
+        Summary = "Get appointment by ID",
+        Description = "Retrieves an appointment by its unique ID.")]
+    [SwaggerResponse(200, "Appointment retrieved successfully", typeof(AppointmentResource))]
+    [SwaggerResponse(404, "Appointment not found")]
+    public async Task<ActionResult> GetAppointmentById(int id)
+    {
+        var getAppointmentByIdQuery = new GetAppointmentByIdQuery(id);
+        var result = await appointmentQueryService.Handle(getAppointmentByIdQuery);
+        if (result is null) return NotFound();
+        return Ok(AppointmentResourceFromEntityAssembler.ToResourceFromEntity(result));
     }
 }
