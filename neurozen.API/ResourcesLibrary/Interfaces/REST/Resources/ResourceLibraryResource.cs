d283d582fbe20aec8ef12b43ef510a5e6514d3a9
Diegoboz <diegomnk@hotmail.com>
2025-11-13 02:06:53 -0500

    feat: implement ResourceLibrary management with command service, repository, and configuration

---
+++ neurozen.API/ResourcesLibrary/Interfaces/REST/Resources/ResourceLibraryResource.cs
@@ -0,0 +1,11 @@
+﻿namespace neurozen.API.ResourcesLibrary.Interfaces.REST.Resources;
+
+public record ResourceLibraryResource(
+    int Id,
+    string Title,
+    string Description,
+    string ResourceType,
+    string ContentUrl,
+    long Duration,
+    string Author
+);
