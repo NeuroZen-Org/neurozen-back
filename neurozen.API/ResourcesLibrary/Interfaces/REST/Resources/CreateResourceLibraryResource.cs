bd2739c52679d7a60d55347c765e0722988f6b23
Diegoboz <diegomnk@hotmail.com>
2025-11-13 02:07:23 -0500

    feat: add CreateResourceLibraryResource record for resource library creation

---
+++ neurozen.API/ResourcesLibrary/Interfaces/REST/Resources/CreateResourceLibraryResource.cs
@@ -0,0 +1,10 @@
+﻿namespace neurozen.API.ResourcesLibrary.Interfaces.REST.Resources;
+
+public record CreateResourceLibraryResource(
+    string Title,
+    string Description,
+    string ResourceType,
+    string ContentUrl,
+    long Duration,
+    string Author
+);
