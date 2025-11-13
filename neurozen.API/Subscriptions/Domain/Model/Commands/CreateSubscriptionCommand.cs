30aac44bcf31dd77d588aecbf2dbc15c18c57c04
Yonyu Gamer <yonyugmer8@gmail.com>
2025-11-13 15:16:50 -0500

    feat: add subscriptionsCommand feature with CRUD endpoints

---
+++ neurozen.API/Subscriptions/Domain/Model/Commands/CreateSubscriptionCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace neurozen.API.Subscriptions.Domain.Model.Commands;
+
+public record CreateSubscriptionCommand(int UserId,int PlanId, string NameUser, string LastNameUser, string EmailUser, string NumberCard, string ExpirationDate, string Cvv, bool IsActive);
