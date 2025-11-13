5f812b12c4c86464d479496808d1170bb29f6eaf
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-12 22:55:57 -0500

    feat: add TriggerRepository, TriggerResource, and TriggersController for trigger management

---
+++ neurozen.API/Triggers/Interfaces/REST/Transform/TriggerResourceFromEntityAssembler.cs
@@ -0,0 +1,17 @@
+﻿using neurozen.API.Triggers.Domain.Model.Aggregates;
+using neurozen.API.Triggers.Interfaces.REST.Resources;
+
+namespace neurozen.API.Triggers.Interfaces.REST.Transform;
+
+public class TriggerResourceFromEntityAssembler
+{
+    public static TriggerResource ToResourceFromEntity(Trigger entity) =>
+        new TriggerResource(
+            entity.Id,
+            entity.PatientId,
+            entity.CategoryId,
+            entity.StressLevel,
+            entity.TriggerDateTime,
+            entity.Description
+        );
+}
