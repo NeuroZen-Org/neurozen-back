f29ed945c621506fb4aaaf6424300233981402eb
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-12 22:55:36 -0500

    feat: add ITriggerCommandService and ITriggerRepository interfaces for trigger management

---
+++ neurozen.API/Triggers/Domain/Services/ITriggerCommandService.cs
@@ -0,0 +1,9 @@
+﻿using neurozen.API.Triggers.Domain.Model.Aggregates;
+using neurozen.API.Triggers.Domain.Model.Commands;
+
+namespace neurozen.API.Triggers.Domain.Services;
+
+public interface ITriggerCommandService
+{
+    Task<Trigger?> Handle(CreateTriggerCommand command);
+}
