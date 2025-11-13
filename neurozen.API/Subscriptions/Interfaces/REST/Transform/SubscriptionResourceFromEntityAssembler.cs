c91e62984086227ffd05d533868a348f8b1f554e
Yonyu Gamer <yonyugmer8@gmail.com>
2025-11-13 15:14:31 -0500

    feat: add subscriptionsResource feature with CRUD endpoints

---
+++ neurozen.API/Subscriptions/Interfaces/REST/Transform/SubscriptionResourceFromEntityAssembler.cs
@@ -0,0 +1,20 @@
+﻿using neurozen.API.Subscriptions.Domain.Model.Aggregates;
+using neurozen.API.Subscriptions.Interfaces.REST.Resources;
+
+namespace neurozen.API.Subscriptions.Interfaces.REST.Transform;
+
+public class SubscriptionResourceFromEntityAssembler
+{
+    public static SubscriptionResource ToResourceFromEntity(Subscription entity) =>
+        new SubscriptionResource(
+            entity.UserId,     // 👈 agregado
+            entity.PlanId,
+            entity.NameUser,
+            entity.LastNameUser,
+            entity.EmailUser,
+            entity.NumberCard,
+            entity.ExpirationDate,
+            entity.Cvv,
+            entity.IsActive ?? false
+        );
+}
