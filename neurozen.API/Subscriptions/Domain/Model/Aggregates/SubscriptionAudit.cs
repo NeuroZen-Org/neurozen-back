e1b8afbd3b54a00f2b67403b5db1d9a026979c78
Yonyu Gamer <yonyugmer8@gmail.com>
2025-11-13 15:15:14 -0500

    feat: add subscriptions feature with CRUD endpoints

---
+++ neurozen.API/Subscriptions/Domain/Model/Aggregates/SubscriptionAudit.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using neurozen.API.Shared.Domain.Repositories;
+
+namespace neurozen.API.Subscriptions.Domain.Model.Aggregates;
+
+public class SubscriptionAudit : IEntityWithCreatedUpdatedDated
+{
+    [Column ("CreatedAt")] public DateTimeOffset CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset UpdatedDate { get; set; }
+}
