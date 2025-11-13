ab01d9c39a5f650120b96862993340b427690ab8
Diegoboz <diegomnk@hotmail.com>
2025-11-13 02:07:13 -0500

    feat: register ResourceLibrary services in the application container

--- neurozen.API/Program.cs
+++ neurozen.API/Program.cs
@@ -12,6 +12,10 @@ using neurozen.API.Triggers.Infraestructure.Respositories;
 using neurozen.API.Shared.Domain.Repositories;
 using neurozen.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using neurozen.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using neurozen.API.ResourcesLibrary.Application.Internal.CommandServices;
+using neurozen.API.ResourcesLibrary.Domain.Repositories;
+using neurozen.API.ResourcesLibrary.Domain.Services;
+using neurozen.API.ResourcesLibrary.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -52,6 +56,10 @@ builder.Services.AddScoped<IAppointmentQueryService, AppointmentQueryService>();
 // Triggers services
 builder.Services.AddScoped<ITriggerRepository, TriggerRepository>();
 builder.Services.AddScoped<ITriggerCommandService, TriggerCommandService>();
+
+// ResourceLibrary Services
+builder.Services.AddScoped<IResourceLibraryCommandService, ResourceLibraryCommandService>();
+builder.Services.AddScoped<IResourceLibraryRepository, ResourceLibraryRepository>();
 
 var app = builder.Build();
 
