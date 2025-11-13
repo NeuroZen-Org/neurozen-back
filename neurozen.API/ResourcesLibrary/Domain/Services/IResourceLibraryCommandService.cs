af027605ef40ab7e10a112ef934b293b43c3fff8
Diegoboz <diegomnk@hotmail.com>
2025-11-13 02:07:19 -0500

    feat: add IResourceLibraryCommandService interface for handling resource library creation commands

---
+++ neurozen.API/ResourcesLibrary/Domain/Services/IResourceLibraryCommandService.cs
@@ -0,0 +1,9 @@
+﻿using neurozen.API.ResourcesLibrary.Domain.Model.Aggregates;
+using neurozen.API.ResourcesLibrary.Domain.Model.Commands;
+
+namespace neurozen.API.ResourcesLibrary.Domain.Services;
+
+public interface IResourceLibraryCommandService
+{
+    Task<ResourceLibrary?> Handle(CreateResourceLibraryCommand command);
+}
