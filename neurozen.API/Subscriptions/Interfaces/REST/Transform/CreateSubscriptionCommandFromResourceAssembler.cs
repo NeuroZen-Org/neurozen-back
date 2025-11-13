f70e7374ebe355eff4e34a9b05522f9905be1e2a
Yonyu Gamer <yonyugmer8@gmail.com>
2025-11-13 15:16:50 -0500

    feat: add subscriptionsCommand feature with CRUD endpoints

---
+++ neurozen.API/Subscriptions/Interfaces/REST/Transform/CreateSubscriptionCommandFromResourceAssembler.cs
@@ -0,0 +1,20 @@
+﻿using neurozen.API.Subscriptions.Domain.Model.Commands;
+using neurozen.API.Subscriptions.Interfaces.REST.Resources;
+
+namespace neurozen.API.Subscriptions.Interfaces.REST.Transform;
+
+public class CreateSubscriptionCommandFromResourceAssembler
+{
+    public static CreateSubscriptionCommand ToCommandFromResource(CreateSubscriptionResource resource) =>
+        new CreateSubscriptionCommand(
+            resource.UserId,   // 👈 agregado
+            resource.PlanId,
+            resource.NameUser,
+            resource.LastNameUser,
+            resource.EmailUser,
+            resource.NumberCard,
+            resource.ExpirationDate,
+            resource.Cvv,
+            resource.IsActive
+        );
+}
