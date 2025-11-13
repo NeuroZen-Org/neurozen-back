0efb5a303ef6c4e166b3ec94b0e6ea0344645ec3
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-13 00:31:11 -0500

    feat: extend CreateAppointment command and resource to include AppointmentType and additional notes

--- neurozen.API/Appointments/Interfaces/REST/Resources/CreateAppointmentResource.cs
+++ neurozen.API/Appointments/Interfaces/REST/Resources/CreateAppointmentResource.cs
@@ -1,3 +1,12 @@
-﻿namespace neurozen.API.Appointments.Interfaces.REST.Resources;
+﻿using System.ComponentModel.DataAnnotations;
+using neurozen.API.Appointments.Domain.Model.ValueObjects;
 
-public record CreateAppointmentResource(int PatientId, int ProfessionalId, DateTime AppointmentDateTime);
+namespace neurozen.API.Appointments.Interfaces.REST.Resources;
+
+public record CreateAppointmentResource(
+    int PatientId, 
+    int ProfessionalId, 
+    DateTime AppointmentDateTime,
+    [Range(1,3, ErrorMessage = "AppointmentType must be between 1 and 3")]
+    EAppointmentType AppointmentType,
+    string? NotasAdicionales = null);
