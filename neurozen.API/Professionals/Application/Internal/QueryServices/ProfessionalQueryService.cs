71add2d3465ebc3e988ff3c75776798be60bd6ea
Trevor <valentinosolischang@gmail.com>
2025-11-13 00:09:53 -0500

    feat: add ProfessionalQueryService for handling GetAllProfessionalsQuery

---
+++ neurozen.API/Professionals/Application/Internal/QueryServices/ProfessionalQueryService.cs
@@ -0,0 +1,14 @@
+﻿using neurozen.API.Professionals.Domain.Model.Aggregates;
+using neurozen.API.Professionals.Domain.Model.Queries;
+using neurozen.API.Professionals.Domain.Repositories;
+using neurozen.API.Professionals.Domain.Services;
+
+namespace neurozen.API.Professionals.Application.Internal.QueryServices;
+
+public class ProfessionalQueryService(IProfessionalRepository professionalRepository) : IProfessionalQueryService
+{
+    public async Task<IEnumerable<Professional>> Handle(GetAllProfessionalsQuery query)
+    {
+        return await professionalRepository.GetAllProfessionals();
+    }
+}
