4e3d1a58a5c7bd6f8c1d579c29134e4a7e4cb203
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-12 22:55:28 -0500

    feat: add TriggerConfiguration to AppDbContext and remove unused properties from AppointmentAudit

--- neurozen.API/Appointments/Domain/Model/Aggregates/AppointmentAudit.cs
+++ neurozen.API/Appointments/Domain/Model/Aggregates/AppointmentAudit.cs
@@ -7,7 +7,4 @@ public partial class AppointmentAudit : IEntityWithCreatedUpdatedDated
 {
     [Column ("CreatedAt")] public DateTimeOffset CreatedDate { get; set; }
     [Column("UpdatedAt")] public DateTimeOffset UpdatedDate { get; set; }
-    [Column ("PatientId")] public long PatientId { get; private set; }
-    [Column ("ProfessionalId")] public long ProfessionalId { get; private set; }
-    [Column ("AppointmentDate")] public DateTime AppointmentDate { get; private set; }
 }
