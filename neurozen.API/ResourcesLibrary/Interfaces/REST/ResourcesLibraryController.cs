709f00495f01a51804c278f8d2e0145d8afff359
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-13 16:22:19 -0500

    feat: add localization support for error messages in ResourcesLibraryController

--- neurozen.API/ResourcesLibrary/Interfaces/REST/ResourcesLibraryController.cs
+++ neurozen.API/ResourcesLibrary/Interfaces/REST/ResourcesLibraryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using neurozen.API.Resources;
 using neurozen.API.ResourcesLibrary.Domain.Model.Commands;
 using neurozen.API.ResourcesLibrary.Domain.Repositories;
 using neurozen.API.ResourcesLibrary.Domain.Services;
@@ -14,23 +16,27 @@ public class ResourceLibrariesController : ControllerBase
 {
     private readonly IResourceLibraryCommandService _commandService;
     private readonly IResourceLibraryRepository _repository;
+    private readonly IStringLocalizer<SharedResource> _localizer;
 
     public ResourceLibrariesController(
         IResourceLibraryCommandService commandService,
-        IResourceLibraryRepository repository)
+        IResourceLibraryRepository repository,
+        IStringLocalizer<SharedResource> localizer)
     {
         _commandService = commandService;
         _repository = repository;
+        _localizer = localizer;
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateResourceLibrary([FromBody] CreateResourceLibraryResource resource)
     {
+        String msg = _localizer.GetString("CreateResourceLibraryError");
         var command = CreateResourceLibraryCommandFromResourceAssembler.ToCommandFromResource(resource);
         var result = await _commandService.Handle(command);
 
         if (result is null)
-            return BadRequest();
+            return BadRequest(new{ message = msg });
 
         var resourceLibraryResource = ResourceLibraryResourceFromEntityAssembler.ToResourceFromEntity(result);
         return CreatedAtAction(nameof(GetResourceLibraryById), new { id = result.Id }, resourceLibraryResource);
@@ -39,10 +45,11 @@ public class ResourceLibrariesController : ControllerBase
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetResourceLibraryById(int id)
     {
+        String msg = _localizer.GetString("GetResourceLibraryByIdError");
         var result = await _repository.FindByIdAsync(id);
 
         if (result is null)
-            return NotFound();
+            return NotFound(new { message = msg });
 
         var resource = ResourceLibraryResourceFromEntityAssembler.ToResourceFromEntity(result);
         return Ok(resource);
@@ -51,7 +58,9 @@ public class ResourceLibrariesController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> GetAllResourceLibraries()
     {
+        String msg = _localizer.GetString("GetAllResourceLibrariesError");
         var results = await _repository.ListAsync();
+        if (results == null || !results.Any()) return NotFound(new { message = msg });
         var resources = results.Select(ResourceLibraryResourceFromEntityAssembler.ToResourceFromEntity);
         return Ok(resources);
     }
