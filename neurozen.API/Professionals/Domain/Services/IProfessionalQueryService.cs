3d50b2d4bcfecdebc51e005b6cb51598ba19b75a
Trevor <valentinosolischang@gmail.com>
2025-11-13 00:10:16 -0500

    feat: implement IProfessionalQueryService interface for querying professionals

---
+++ neurozen.API/Professionals/Domain/Services/IProfessionalQueryService.cs
@@ -0,0 +1,9 @@
+﻿using neurozen.API.Professionals.Domain.Model.Aggregates;
+using neurozen.API.Professionals.Domain.Model.Queries;
+
+namespace neurozen.API.Professionals.Domain.Services;
+
+public interface IProfessionalQueryService
+{
+    Task<IEnumerable<Professional>> Handle(GetAllProfessionalsQuery query);
+}
